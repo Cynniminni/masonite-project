386b6e9e7b2d13477e0bcf75cf9c420a4d014c5a
Cynniminni <radicalcyn@gmail.com>
2022-03-31 19:51:17 -0700

    Add nickname and handle to user table

--- app/controllers/HomeController.py
+++ app/controllers/HomeController.py
@@ -1,7 +1,18 @@
 from masonite.controllers import Controller
 from masonite.views import View
+from app.models.Post import Post
 
 
 class HomeController(Controller):
     def show(self, view: View):
-        return view.render("home")
+        """
+        Get all the posts and pass them to the home "/" page.
+        """
+        # Get all posts
+        all_posts = Post.all()
+        all_posts = {
+            'posts': all_posts
+        }
+
+        # Render the view called posts.html and pass in the posts data
+        return view.render("home", all_posts)
