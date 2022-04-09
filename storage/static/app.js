29eb77f647b9763021f40992b49cbe5e626cc2c7
Cynniminni <radicalcyn@gmail.com>
2022-04-09 13:06:26 -0700

    Remove adding/removing mt-14 class in app.js

--- storage/static/app.js
+++ storage/static/app.js
@@ -7,11 +7,7 @@ burger.addEventListener('click', () => {
     // Check if the menu has the class 'hidden'
     if (menu.classList.contains('hidden')) {
         menu.classList.remove('hidden'); // Show the mobile menu
-        if (post_block.classList.contains("mt-14")) {
-            post_block.classList.remove("mt-14"); // Remove margin top so there's no gaps below mobile menu
-        }
     } else {
         menu.classList.add('hidden'); // Hide mobile menu
-        post_block.classList.add("mt-14"); // Bump post block to be beneath header
     }
 });
