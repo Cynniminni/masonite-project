577c62b552701e8fb6ca36850ffc65bb3aee874c
Cynniminni <radicalcyn@gmail.com>
2022-04-09 23:47:50 -0700

    Disable button when textarea is empty, enable when not empty

--- storage/static/app.js
+++ storage/static/app.js
@@ -1,3 +1,6 @@
+//-------------------------
+// Hide/Show mobile menu when clicking burger icon
+//-------------------------
 const burger = document.querySelector("#burger");
 const menu = document.querySelector("#menu");
 const post_block = document.querySelector("#post-block");
@@ -11,3 +14,29 @@ burger.addEventListener('click', () => {
         menu.classList.add('hidden'); // Hide mobile menu
     }
 });
+
+//-------------------------
+// Disable Submit button until text is entered
+//-------------------------
+const submit_button = document.querySelector("#submit-button");
+let text_area = document.querySelector("#text-area");
+
+text_area.addEventListener('keyup', () => {
+    // If the <textarea> has text in it
+    if (text_area.value.trim() != '') {
+        // Remove 50% opacity class
+        if (submit_button.classList.contains("opacity-50")) {
+            submit_button.classList.remove("opacity-50");
+        }
+        // Enable the submit button
+        submit_button.disabled = false;
+    } else {
+        // Add 50% opacity class
+        if (!submit_button.classList.contains("opacity-50")) {
+            submit_button.classList.add("opacity-50");
+        }
+        // Disable the submit button
+        submit_button.disabled = true;
+    }
+});
+
