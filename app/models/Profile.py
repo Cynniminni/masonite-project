89a696335557f5938466db30c1ab67fea089ee6d
Cynniminni <radicalcyn@gmail.com>
2022-04-10 15:11:58 -0700

    Add fillable fields to Profile model and foreign key definition

--- app/models/Profile.py
+++ app/models/Profile.py
@@ -1,9 +1,20 @@
 """ User Model """
 
 from masoniteorm.models import Model
-
+from masoniteorm.relationships import belongs_to
 
 class Profile(Model):
     """Profile Model"""
+    __fillable__ = [
+        "user_id",
+        "nickname",
+        "handle",
+        "bio",
+        "picture",
+        "banner"
+    ]
 
-    pass
+    @belongs_to('user_id', 'id')
+    def author(self):
+        from app.models.User import User
+        return User
