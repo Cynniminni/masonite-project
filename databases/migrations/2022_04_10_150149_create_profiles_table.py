a862ab7319344a02172bbfe3087f7646a8085413
Cynniminni <radicalcyn@gmail.com>
2022-04-10 15:08:47 -0700

    Add profiles table migration

---
+++ databases/migrations/2022_04_10_150149_create_profiles_table.py
@@ -0,0 +1,27 @@
+"""CreateProfilesTable Migration."""
+
+from masoniteorm.migrations import Migration
+
+
+class CreateProfilesTable(Migration):
+    def up(self):
+        """
+        Run the migrations.
+        """
+        with self.schema.create("profiles") as table:
+            table.increments("id")
+            table.integer("user_id").unsigned()
+            table.foreign("user_id").references("id").on("users")
+
+            table.string("nickname")
+            table.string("handle")
+            table.string("bio")  # description
+            table.string("picture")  # profile picture
+            table.string("banner")  # profile banner
+            table.timestamps()
+
+    def down(self):
+        """
+        Revert the migrations.
+        """
+        self.schema.drop("profiles")
